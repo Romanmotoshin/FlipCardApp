ca851043c0e5ebd3e14cab0e37dcd9945f1b6771
roman <roman.motoshin@yandex.ru>
2023-01-09 13:06:29 +0300

    all done

--- src/components/mainMenuCard/MainMenuCard.js
+++ src/components/mainMenuCard/MainMenuCard.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 const MainMenuCard = ({id}) => {
 
     const [turn, setTurn] = useState(1)
-    const [event, setEvent] = useState(false)
     const [clazz, setClazz] = useState({clazzFront: null, clazzBack: null})
 
     useEffect(() => {
@@ -33,65 +32,50 @@ const MainMenuCard = ({id}) => {
                     'transition': `transform 4s cubic-bezier(0.23, 3, 0.2, -0.3)`,
                 }
             }))
-            setTimeout(() => {
-                setEvent(true)
-            }, 4000)
         }, 8200 + (21 - id) * 100)
     }, [])
 
-    useEffect(() => {
-        setEvent(true)
-    }, [turn])
-
     const onHover = () => {
-        if (event) {
-            setClazz(({
-                clazzFront: {
-                    'transform': `rotateY(0.5turn)`,
-                    'transition': `transform 1s linear`,
-                },
-                clazzBack: {
-                    'transform': `rotateY(0turn)`,
-                    'transition': `transform 1s linear`,
-                }
-            }))
-        }
+        setClazz(({
+            clazzFront: {
+                'transform': `rotateY(0.5turn)`,
+                'transition': `transform 1s linear`,
+            },
+            clazzBack: {
+                'transform': `rotateY(0turn)`,
+                'transition': `transform 1s linear`,
+            }
+        }))
     }
 
     const onLeave = () => {
-        if (event) {
-            setClazz(({
-                clazzFront: {
-                    'transform': 'rotateY(0turn)',
-                    'transition': `transform 1s linear`,
-                },
-                clazzBack: {
-                    'transform': 'rotateY(-0.5turn)',
-                    'transition': `transform 1s linear`,
-                }
-            }))
-            setTurn(1)
-        }
+        setClazz(({
+            clazzFront: {
+                'transform': 'rotateY(0turn)',
+                'transition': `transform 1s linear`,
+            },
+            clazzBack: {
+                'transform': 'rotateY(-0.5turn)',
+                'transition': `transform 1s linear`,
+            }
+        }))
+        setTurn(1)
     }
 
     const onFaster = () => {
-        setEvent(false)
-        if (event) {
-            setClazz(({
-                clazzFront: {
-                    'transform': `rotateY(${turn}turn)`,
-                    'transition': `transform 0.2s linear`,
-                },
-                clazzBack: {
-                    'transform': `rotateY(${turn - 0.5}turn)`,
-                    'transition': `transform 0.2s linear`,
-                }
-            }))
-            setTimeout(() => {
-                setTurn(turn => turn + 0.5)
-            }, 200)
-            
-        }
+        setClazz(({
+            clazzFront: {
+                'transform': `rotateY(${turn}turn)`,
+                'transition': `transform 0.2s linear`,
+            },
+            clazzBack: {
+                'transform': `rotateY(${turn - 0.5}turn)`,
+                'transition': `transform 0.2s linear`,
+            }
+        }))
+        setTimeout(() => {
+            setTurn(turn => turn + 0.5)
+        }, 200)
     }
 
     return ( 
